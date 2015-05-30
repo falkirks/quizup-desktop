2656e2fdbc8a9d5ce307473eff74175e55f955cb
Falkirks <me@falkirks.com>
2015-05-30 11:05:34 -0700

    Autoloading of CSS and JS, improved settings pane

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ const app = require('app');
 const BrowserWindow = require('browser-window');
 var ipc = require('ipc');
 var fs = require("fs");
-
+String.prototype.endsWith = function(suffix) {
+	return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+var walk = function(dir) {
+	var results = [];
+	var list = fs.readdirSync(dir);
+	list.forEach(function(file) {
+		file = dir + '/' + file;
+		var stat = fs.statSync(file);
+		if (stat && stat.isDirectory()) results = results.concat(walk(file));
+		else results.push(file)
+	});
+	return results
+};
+var files = walk(__dirname + "/lib");
+var scripts = [];
+var styles = [];
+for(var i = 0; i < files.length; i++){
+	if(files[i].endsWith(".js")){
+		scripts.push(files[i]);
+	}
+	else if(files[i].endsWith(".css")){
+		styles.push(files[i]);
+	}
+}
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -32,11 +56,15 @@ app.on('ready', function () {
 		mainWindow = null;
 	});
 	mainWindow.webContents.on('dom-ready', function () {
-		mainWindow.webContents.executeJavaScript(fs.readFileSync(__dirname + "/page.js", "utf8"));
 		//mainWindow.webContents.insertCSS(fs.readFileSync(__dirname + "/page.css", "utf8"));
 	});
-	mainWindow.webContents.on('did-frame-finish-load', function(){
-		mainWindow.webContents.insertCSS(fs.readFileSync(__dirname + "/page.css", "utf8"));
+	mainWindow.webContents.on('did-finish-load', function(){
+		for(var i = 0; i < scripts.length; i++) {
+			mainWindow.webContents.executeJavaScript(fs.readFileSync(scripts[i], "utf8"));
+		}
+		for(var j = 0; j < styles.length; j++){
+			mainWindow.webContents.insertCSS(fs.readFileSync(styles[j], "utf8"));
+		}
 	});
 	ipc.on('asynchronous-message', function (event, arg) {
 		console.log(arg);  // prints "ping"
